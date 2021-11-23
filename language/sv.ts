481e2d79487da89308ada50bff5ad9cda28497bd
udde97 <maab970514@student.jenseneducation.se>
2021-11-23 14:17:30 +0100

    Added translator

---
+++ language/sv.ts
@@ -0,0 +1,6 @@
+import { tokens } from "./appStructure";
+export const swedish = {
+    [tokens.screens.app.MainText]:"Välkommen till min app!",
+
+    
+    [tokens.screens.screen1.Button]: "Tryck här"};
