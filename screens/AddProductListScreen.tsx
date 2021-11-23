6cf85288cbbf01cd7c90d3828b51b76afb869688
Elmir Abasov <56082840+Xizafren@users.noreply.github.com>
2021-11-23 20:12:59 +0100

    Last commit 23/11

--- screens/AddProductListScreen.tsx
+++ screens/AddProductListScreen.tsx
@@ -1,23 +1,67 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, { useContext } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { ProductsContext } from "../components/context/provider";
+import { StackScreens } from "../components/helpers/types";
 
-const AddProductListScreen = () => {
-     const [enteredValue, setEnteredValue] = React.useState("");
+export const AddProductListScreen: React.FC<
+  NativeStackScreenProps<StackScreens, "AddProductListScreen">
+ = (props) => {
+
+  const { products, addProduct } = useContext(ProductsContext);
+  const [Name, setName] = React.useState("");
+  const [Price, setPrice] = React.useState("");
+  const [Type, setType] = React.useState("");
   return (
     <View style={styles.container}>
-      <Text
-        style={[styles.width80, styles.margin10]}
-        
-      > First Name </Text>
-      <Text
-        style={[styles.width80, styles.margin10]}>
-        
-        Price </Text>
-      <Text
-       
-        style={[styles.width80, styles.margin10]}>
-            Product Type
-      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setName}
+        value={Name}
+        placeholder="useless placeholder"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setPrice}
+        value={Price}
+        placeholder="useless placeholder"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setType}
+        value={Type}
+        placeholder="useless placeholder"
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          flex: 1,
+          justifyContent: "space-around",
+          width: "100%",
+        }}
+      >
+        <Button
+          onPress={() => {
+            addProduct({
+              id: products.length + 1,
+              name: Name,
+              price: Number(Price),
+              type: Number(Type),
+            });
+            props.navigation.navigate("ProductListScreen")
+          }}
+          title="SAVE"
+          color="green"
+        />
+        <Button
+          onPress={() => {
+            ("");
+          }}
+          title="CANCEL"
+          color="red"
+        />
+      </View>
     </View>
   );
 };
@@ -25,17 +69,20 @@ const AddProductListScreen = () => {
 export default AddProductListScreen;
 
 const styles = StyleSheet.create({
-   width80: {
-    width: 80
-   },
-   margin10: {
-       margin: 10
-   },
+  input: {
+    color: "black",
+    height: 60,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: "90%",
+  },
+  buttonStyle: {
+    marginHorizontal: 10,
+  },
 
   container: {
-    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
   },
-  
 });
