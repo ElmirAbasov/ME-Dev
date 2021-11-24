28049bf0465648d5ba5432c87cfce2603c76bdd6
udde97 <maab970514@student.jenseneducation.se>
2021-11-24 15:31:53 +0100

    added language variables

--- language/en.ts
+++ language/en.ts
@@ -1,6 +1,26 @@
 import { tokens } from "./appStructure";
 export const english = {
     [tokens.screens.app.MainText]:"Welcome To My App",
-    [tokens.screens.screen1.MainText]: "This is from Component",
-    [tokens.screens.screen1.Button]: "Click Here"
+
+
+
+    [tokens.screens.addProductListScreen.MainText]: "Create new",
+    [tokens.screens.addProductListScreen.Cancel]:  "Cancel",
+    [tokens.screens.addProductListScreen.Save]: "Save",
+
+
+    [tokens.screens.productListScreen.MainText]:"Products",
+    [tokens.screens.productListScreen.Name]: "Name",
+    [tokens.screens.productListScreen.Price]: "Price",
+    [tokens.screens.productListScreen.Type]: "Type",
+
+
+
+
+
+
+
+
+    
+
 };
